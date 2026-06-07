cd48810af55d6a0f853d9007bf273bd89305f01e
Agustin Jeya Kumar <jeyak592@gmail.com>
2026-06-07 13:27:33 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-sQ5DmKoR.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Jk/Downloads/elyzion-elevate-main/elyzion-elevate-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BCZxb6RM.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BCZxb6RM.js" } }] }, "/": { filePath: "C:/Users/Jk/Downloads/elyzion-elevate-main/elyzion-elevate-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-De4JgwCv.js"] } } });
+export {
+  tsrStartManifest
+};
